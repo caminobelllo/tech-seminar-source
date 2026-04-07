db04f1d547c2a70cbd5ae29aa97ba366ecf8ff1e
caminobelllo <alstpgml0812@gmail.com>
2026-04-08 01:32:15 +0900

    Test: 정상 배포 테스트 - console log

--- src/index.js
+++ src/index.js
@@ -164,3 +164,5 @@ app.get('/health', (req, res) => {
 app.listen(3000, () => {
   console.log(`[${ENV}] ${SERVICE} ${VERSION} 시작됨 → http://localhost:3000/status`);
 });
+
+console.log("정상 배포 테스트");
