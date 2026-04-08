acdd4fd886c58025243fd917688895a8ecc9c963
caminobelllo <alstpgml0812@gmail.com>
2026-04-09 00:19:02 +0900

    Chore: 오류 코드 롤백

--- src/index.js
+++ src/index.js
@@ -164,7 +164,3 @@ app.get('/health', (req, res) => {
 app.listen(3000, () => {
   console.log(`[${ENV}] ${SERVICE} ${VERSION} 시작됨 → http://localhost:3000/status`);
 });
-
-
-console.log("장애 배포 테스트 ..... 77");
-process.exit(1);
